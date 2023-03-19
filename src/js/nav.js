7ff67cd9d5434b282d9745cbde8149128386d8a0
DaveAdbeel <davidadbeelgonzalez@gmail.com>
2023-03-19 14:05:54 -0300

    nav and popular section done! :D

---
+++ src/js/nav.js
@@ -0,0 +1,12 @@
+const nav_menu = document.querySelector(".nav_menu");
+const nav_list = document.querySelector(".nav_list");
+const body = document.querySelector("body");
+
+nav_menu.addEventListener("click", () => {
+   nav_list.classList.toggle("nav_list--show");
+   body.classList.toggle("body--show");
+   const nav_menu_state = nav_menu.getAttribute("src");
+   nav_menu_state === "./assets/images/icon-menu.svg"
+      ? nav_menu.setAttribute("src", "./assets/images/icon-menu-close.svg")
+      : nav_menu.setAttribute("src", "./assets/images/icon-menu.svg");
+});
